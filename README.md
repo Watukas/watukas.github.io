cdd21ba251054490a43698bfe6c87d205fec4474
Vaapukkax <73548405+Watukas@users.noreply.github.com>
2025-07-05 14:17:50 +0300

    Keep it clean and simple

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# watukas.github.io
-watukas.dev
+<br/>
+<div align="center">
+  <h3>watukas.dev</h3>
+  <a href="https://watukas.dev">watukas.dev</a>
+</div>
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import logo from '/favicon.png'
+import './App.css'
+
+function App() {
+  return (
+    <div id="title">
+      <img src={logo} className="logo" alt="Watukas" />
+      { /*<h1>Watukas</h1>*/ }
+    </div>
+  )
+}
+
+export default App
